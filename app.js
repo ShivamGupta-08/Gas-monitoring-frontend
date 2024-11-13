3702e929eabc1723aea413a60d4a56c71a3258a9
Shivam Gupta <87187728+ShivamGupta-08@users.noreply.github.com>
2024-11-13 18:20:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -86,6 +86,10 @@ async function fetchData() {
             document.getElementById('status').innerHTML = "Status: Off";
             return; // Exit the function to prevent further updates
         }
+         // Restart the interval when power is back on
+        if (data.status === "on" && !dataFetchInterval) {
+            dataFetchInterval = setInterval(fetchData, 2000); // Restart the interval
+        }
 
         // Update DOM elements with the latest data
         document.getElementById('helmet-id').textContent = data.helmetId;
